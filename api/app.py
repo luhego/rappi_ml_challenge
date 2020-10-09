e81194a33b676326a350e0e56dce1fa65880eae5
Luis Herrera <luhegoes@gmail.com>
2020-10-09 04:51:49 -0500

    Added logging to api

--- api/app.py
+++ api/app.py
@@ -1,6 +1,9 @@
 from flask import Flask, jsonify, request
 
 from predict import run_prediction
+from pipeline.logger import setup_logger
+
+logger = setup_logger(__name__)
 
 app = Flask(__name__)
 
@@ -8,8 +11,11 @@ app = Flask(__name__)
 @app.route("/predict", methods=["POST"])
 def predict():
     data = request.json
+    logger.info(f"API predict with payload {data}.")
 
     prediction = run_prediction(data)
+
+    logger.info(f"API predictions {prediction}.")
 
     return jsonify({"data": prediction})
 
